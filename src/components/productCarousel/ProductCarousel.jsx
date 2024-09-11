2d8c375c697ab9c5aecc6d63406ad25967189e4d
Facundo Quiroga <facu.quirogaga@gmail.com>
2024-09-11 11:41:40 -0500

    Terminado el responsive

--- src/components/productCarousel/ProductCarousel.jsx
+++ src/components/productCarousel/ProductCarousel.jsx
@@ -6,20 +6,20 @@ const ProductCarousel = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
           slidesToScroll: 1,
         }
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
         }
       },
@@ -28,6 +28,8 @@ const ProductCarousel = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          centerMode: true, // Centra el producto en pantallas pequeñas
+          dots: true, // Mantiene los puntos de navegación
         }
       }
     ]
